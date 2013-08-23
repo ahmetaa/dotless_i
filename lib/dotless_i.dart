90213c8f1c5136aab9354ba840318f57cad0010f
mdakin <mdakin@gmail.com>
2013-08-23 23:37:37 +0200

    Add TURKISH_STRING_COMPARATOR_IGNORE_CASE comparator. Minor documentation fixes.

--- lib/dotless_i.dart
+++ lib/dotless_i.dart
@@ -14,20 +14,20 @@ const LATIN_SMALL_LETTER_I = "i";
  */
 String toUpperCaseTr(String input) {
   var buffer = new StringBuffer();
-  List<int> toAppend = new List<int>(); 
+  List<int> toAppend = new List<int>();
   for(int codeUnit in input.codeUnits) {
     if(codeUnit==LATIN_SMALL_LETTER_DOTLESS_I_CODE) {
       if(toAppend.length>0){
         buffer.write(new String.fromCharCodes(toAppend).toUpperCase());
         toAppend.clear();
       }
-      buffer.write(LATIN_CAPITAL_LETTER_I);      
+      buffer.write(LATIN_CAPITAL_LETTER_I);
     }
     else if(codeUnit==LATIN_SMALL_LETTER_I_CODE) {
       if(toAppend.length>0){
         buffer.write(new String.fromCharCodes(toAppend).toUpperCase());
         toAppend.clear();
-      }      
+      }
       buffer.write(LATIN_CAPITAL_LETTER_I_WITH_DOT_ABOVE);
     } else
     toAppend.add(codeUnit);
@@ -43,25 +43,25 @@ String toUpperCaseTr(String input) {
  */
 String toLowerCaseTr(String input) {
   var buffer = new StringBuffer();
-  List<int> toAppend = new List<int>(); 
+  List<int> toAppend = new List<int>();
   for(int codeUnit in input.codeUnits) {
     if(codeUnit==LATIN_CAPITAL_LETTER_I_WITH_DOT_ABOVE_CODE) {
       if(toAppend.length>0){
         buffer.write(new String.fromCharCodes(toAppend).toLowerCase());
         toAppend.clear();
       }
-      buffer.write(LATIN_SMALL_LETTER_I);      
+      buffer.write(LATIN_SMALL_LETTER_I);
     }
     else if(codeUnit==LATIN_CAPITAL_LETTER_I_CODE) {
       if(toAppend.length>0){
         buffer.write(new String.fromCharCodes(toAppend).toLowerCase());
         toAppend.clear();
-      }      
+      }
       buffer.write(LATIN_SMALL_LETTER_DOTLESS_I);
     } else {
       toAppend.add(codeUnit);
     }
-    
+
   }
   if(toAppend.length>0){
     buffer.write(new String.fromCharCodes(toAppend).toLowerCase());
@@ -70,34 +70,49 @@ String toLowerCaseTr(String input) {
 }
 
 /// Turkish alphabet aware String Comparator.
-final Comparator<String> TURKISH_STRING_COMPARATOR = (String a, String b) => _compareTr(a,b);
+final Comparator<String> TURKISH_STRING_COMPARATOR =
+    (String a, String b) => _compareTr(a, b, false);
+
+/// Case insensitive Turkish alphabet aware String Comparator.
+final Comparator<String> TURKISH_STRING_COMPARATOR_IGNORE_CASE =
+    (String a, String b) => _compareTr(a, b, true);
 
 class _Lookup {
   List<int> orderLookup = new List<int>.filled(0x160, -1);
+  List<int> orderLookupIgnoreCase = new List<int>.filled(0x160, -1);
+
   _Lookup() {
-    // english charcters are included in alphabet.
+    // English charcters are included in alphabet.
     var alphabet = "ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVWXYZabcçdefgğhıijklmnoöpqrsştuüvwxyz";
+    var letterCount = alphabet.length ~/ 2;
     for(int i = 0; i < alphabet.length; ++i) {
-      orderLookup[alphabet.codeUnitAt(i)]=i; 
+      int index = alphabet.codeUnitAt(i);
+      orderLookup[index] = i;
+      orderLookupIgnoreCase[index] = i % letterCount;
     }
   }
-  
-  int getOrder(int codeUnit) => codeUnit > 0x15F ? -1 : orderLookup[codeUnit];  
+
+  int getOrder(int codeUnit) => codeUnit > 0x15F ? -1 : orderLookup[codeUnit];
+  int getOrderIgnoreCase(int codeUnit) => codeUnit > 0x15F ? -1 : orderLookupIgnoreCase[codeUnit];
 }
 
 final _Lookup TR_CODEUNIT_ORDER_LOOKUP = new _Lookup();
 
-/// some code is used from Dart core. 
-int _compareTr(String a, String b) {
+/// Some code is used from Dart core.
+int _compareTr(String a, String b, [bool ignoreCase]) {
   int aLength = a.length;
   int bLength = b.length;
   int len = (aLength < bLength) ? aLength : bLength;
   for (int i = 0; i < len; i++) {
     int aCodePoint = a.codeUnitAt(i);
     int bCodePoint = b.codeUnitAt(i);
-    int aCodePointTr = TR_CODEUNIT_ORDER_LOOKUP.getOrder(aCodePoint);
-    int bCodePointTr = TR_CODEUNIT_ORDER_LOOKUP.getOrder(bCodePoint);
-    if(aCodePointTr>=0 && bCodePointTr>=0) {
+    int aCodePointTr = ignoreCase != true
+        ? TR_CODEUNIT_ORDER_LOOKUP.getOrder(aCodePoint)
+        : TR_CODEUNIT_ORDER_LOOKUP.getOrderIgnoreCase(aCodePoint);
+    int bCodePointTr = ignoreCase != true
+        ? TR_CODEUNIT_ORDER_LOOKUP.getOrder(bCodePoint)
+        : TR_CODEUNIT_ORDER_LOOKUP.getOrderIgnoreCase(bCodePoint);
+    if(aCodePointTr >= 0 && bCodePointTr >= 0) {
       aCodePoint = aCodePointTr;
       bCodePoint = bCodePointTr;
     }
@@ -110,5 +125,5 @@ int _compareTr(String a, String b) {
   }
   if (aLength < bLength) return -1;
   if (aLength > bLength) return 1;
-  return 0;    
+  return 0;
 }
--- test/dotless_i_test.dart
+++ test/dotless_i_test.dart
@@ -6,28 +6,37 @@ import 'package:dotless_i/dotless_i.dart';
 main() {
   test('toUpperCaseTr', () {
     expect('ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ', toUpperCaseTr('abcçdefgğhıijklmnoöprsştuüvyz'));
-    expect('IİJKLMNOÖPRSŞTUÜVYZ', toUpperCaseTr('ıijklmnoöprsştuüvyz'));    
-    expect('ABCÇDEFGĞHI', toUpperCaseTr('abcçdefgğhı'));  
-    expect('I', toUpperCaseTr('ı'));    
-    expect('İ', toUpperCaseTr('i'));    
+    expect('IİJKLMNOÖPRSŞTUÜVYZ', toUpperCaseTr('ıijklmnoöprsştuüvyz'));
+    expect('ABCÇDEFGĞHI', toUpperCaseTr('abcçdefgğhı'));
+    expect('I', toUpperCaseTr('ı'));
+    expect('İ', toUpperCaseTr('i'));
   });
   test('toLowerCaseTr', () {
     expect('abcçdefgğhıijklmnoöprsştuüvyz', toLowerCaseTr('ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ'));
-    expect('ıijklmnoöprsştuüvyz', toLowerCaseTr('IİJKLMNOÖPRSŞTUÜVYZ'));    
-    expect('abcçdefgğhı', toLowerCaseTr('ABCÇDEFGĞHI'));  
-    expect('ı', toLowerCaseTr('I'));    
-    expect('i', toLowerCaseTr('İ'));    
-  });  
-  
+    expect('ıijklmnoöprsştuüvyz', toLowerCaseTr('IİJKLMNOÖPRSŞTUÜVYZ'));
+    expect('abcçdefgğhı', toLowerCaseTr('ABCÇDEFGĞHI'));
+    expect('ı', toLowerCaseTr('I'));
+    expect('i', toLowerCaseTr('İ'));
+  });
+
   test('comparatorTest', () {
-    expect(["d","ç"]..sort(TURKISH_STRING_COMPARATOR), orderedEquals(["ç","d"]));    
-    expect(["ü","ş","ö","i","ı","ğ","ç"]..sort(TURKISH_STRING_COMPARATOR), 
+    expect(["d","ç"]..sort(TURKISH_STRING_COMPARATOR), orderedEquals(["ç","d"]));
+    expect(["ü","ş","ö","i","ı","ğ","ç"]..sort(TURKISH_STRING_COMPARATOR),
         orderedEquals(["ç","ğ","ı","i","ö","ş","ü"]));
-    expect(["Ü","Ş","Ö","İ","I","Ğ","Ç"]..sort(TURKISH_STRING_COMPARATOR), 
-        orderedEquals(["Ç","Ğ","I","İ","Ö","Ş","Ü"]));    
-    expect(["ü","ş","ö","i","ı","ğ","ç", "Ü","Ş","Ö","İ","I","Ğ","Ç"]..sort(TURKISH_STRING_COMPARATOR), 
+    expect(["Ü","Ş","Ö","İ","I","Ğ","Ç"]..sort(TURKISH_STRING_COMPARATOR),
+        orderedEquals(["Ç","Ğ","I","İ","Ö","Ş","Ü"]));
+    expect(["ü","ş","ö","i","ı","ğ","ç", "Ü","Ş","Ö","İ","I","Ğ","Ç"]..sort(TURKISH_STRING_COMPARATOR),
         orderedEquals(["Ç","Ğ","I","İ","Ö","Ş","Ü","ç","ğ","ı","i","ö","ş","ü"]));
     // English charcters are considered in alphabet.
-    expect(["ü","q"]..sort(TURKISH_STRING_COMPARATOR), orderedEquals(["q","ü"]));   
-  });   
+    expect(["ü","q"]..sort(TURKISH_STRING_COMPARATOR), orderedEquals(["q","ü"]));
+  });
+
+  test('comparatorTestIgnoreCase', () {
+    expect(["D","ç"]..sort(TURKISH_STRING_COMPARATOR_IGNORE_CASE), orderedEquals(["ç","D"]));
+    var list = ["ü","ş","ö","i","ı","ğ","ç", "Ü","Ş","Ö","İ","I","Ğ","Ç"];
+    list.sort(TURKISH_STRING_COMPARATOR_IGNORE_CASE);
+    expect(toLowerCaseTr(list.toString()), "[ç, ç, ğ, ğ, ı, ı, i, i, ö, ö, ş, ş, ü, ü]");
+    expect(["Ü","Ş","Ö","İ","I","Ğ","Ç"]..sort(TURKISH_STRING_COMPARATOR_IGNORE_CASE),
+        orderedEquals(["Ç","Ğ","I","İ","Ö","Ş","Ü"]));
+  });
 }
